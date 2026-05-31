a057d15d486c93cd2f858b0223eb59555d1e36fd
MazenElmogy14 <mazenelmogy2@gmail.com>
2026-05-31 14:06:08 +0300

    add comment here

--- barber_shop_V5/app.py
+++ barber_shop_V5/app.py
@@ -623,4 +623,4 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host= "0.0.0.0")
+    app.run(debug=False, port=5000, host= "0.0.0.0")
